8e0e4eece89136672946c3a7fa305da5a38fda5e
Kaosisochukwu Uzokwe <kuzokwe@gmail.com>
2015-02-12 13:10:24 -0500

    removed unused imports and changed how connection to mongo was being made to align with mongoengine and also how the data from the DB was being accessed to align with the new model obejct

--- vendorApp/views.py
+++ vendorApp/views.py
@@ -1,62 +1,50 @@
-from django.shortcuts import render
-import django_filters
-from rest_framework import filters
+import os
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
 from rest_framework.decorators import renderer_classes
 from rest_framework.response import Response
 from rest_framework.renderers import JSONRenderer
-from rest_framework.views import APIView
-from pymongo import Connection, DESCENDING
 from models import Article
 from serializers import ArticleSerializer
+from mongoengine import connect
+
+# connect to MongoDB
+conn_uri = os.environ.get('MONGOLAB_URI')
+db = 'vendor'
+if conn_uri is not None:  # production env
+    connect(db, host=conn_uri)
+else:  # development env, using localhost
+    connect(db)
+
 
 @csrf_exempt
 @api_view(['GET'])
 @renderer_classes((JSONRenderer,))
-def articles(request, format=None):
-    #filter_backends = (filters.OrderingFilter,)
-    #ordering_fields = ('index')
-    #connect to our local mongodb
-    db = Connection('localhost',27017)
-    #get a connection to our database
-    dbconn = db.vendor
-    articleCollection = dbconn['articles']
-    articles = []
-    for r in articleCollection.find().sort('index', DESCENDING):
-        article = Article( r["_id"] , r["title"] , r["text"] , r["source"] , r["coverPic"] , r["section"], r["logo"], r["index"])
-        articles.append(article)
-    serializedList = ArticleSerializer(articles, many=True)
-    return Response(serializedList.data)
+def articles(request):
+    results = []
+    for article in Article.objects:
+        results.append(article)
+    serialized_list = ArticleSerializer(results, many=True)
+    return Response(serialized_list.data)
+
 
 @csrf_exempt
 @api_view(['GET'])
+@renderer_classes((JSONRenderer,))
 def popular(request):
-    #connect to our local mongodb
-    db = Connection('localhost',27017)
-    #get a connection to our database
-    dbconn = db.vendor
-    articleCollection = dbconn['articles']
-    articles = []
-    for r in articleCollection.find({'section':'Popular'}).sort('index', DESCENDING):
-        article = Article( r["_id"] , r["title"] , r["text"] , r["source"] , r["coverPic"] , r["section"], r["logo"], r["index"])
-        articles.append(article)
-    serializedList = ArticleSerializer(articles, many=True)
-    return Response(serializedList.data)
+    results = []
+    for article in Article.objects(section='Popular'):
+        results.append(article)
+    serialized_list = ArticleSerializer(results, many=True)
+    return Response(serialized_list.data)
+
 
 @csrf_exempt
 @api_view(['GET'])
+@renderer_classes((JSONRenderer,))
 def tech(request):
-    #connect to our local mongodb
-    db = Connection('localhost',27017)
-    #get a connection to our database
-    dbconn = db.vendor
-    articleCollection = dbconn['articles']
-    articles = []
-    for r in articleCollection.find({'section':'Tech'}).sort('index', DESCENDING):
-        article = Article( r["_id"] , r["title"] , r["text"] , r["source"] , r["coverPic"] , r["section"], r["logo"], r["index"])
-        articles.append(article)
-    serializedList = ArticleSerializer(articles, many=True)
-    return Response(serializedList.data)
-
-
+    results = []
+    for article in Article.objects(section='Tech'):
+        results.append(article)
+    serialized_list = ArticleSerializer(results, many=True)
+    return Response(serialized_list.data)
