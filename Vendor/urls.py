442b5d86c19edde2d322ddd7095c28d304789df7
Kaosisochukwu Uzokwe <kuzokwe@gmail.com>
2015-02-22 17:09:43 -0500

    added API endpoints for liking, disliking and getting individual articles

--- Vendor/urls.py
+++ Vendor/urls.py
@@ -8,8 +8,9 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^articles/$', 'vendorApp.views.get_articles'),
+    url(r'^article/(?P<article_id>[0-9a-fA-F]{24})/$', 'vendorApp.views.get_article'),
     url(r'^articles/popular/$', 'vendorApp.views.get_popular', name='popular'),
     url(r'^articles/tech/$', 'vendorApp.views.get_tech', name='tech'),
-    url(r'^artlice/like/(?P<article_id>[0-9a-fA-F]{24})/$', 'vendorApp.views.like_article', name='home'),
-    url(r'^artlice/dislike/(?P<article_id>[0-9a-fA-F]{24})/$', 'vendorApp.views.dislike_article', name='home'),
+    url(r'^article/like/(?P<article_id>[0-9a-fA-F]{24})/$', 'vendorApp.views.like_article'),
+    url(r'^article/dislike/(?P<article_id>[0-9a-fA-F]{24})/$', 'vendorApp.views.dislike_article'),
 )
