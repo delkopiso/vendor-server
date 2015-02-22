ce4757636748ce02870e3b5a81ed9f095a9685a4
Kaosisochukwu Uzokwe <kuzokwe@gmail.com>
2015-02-22 15:11:55 -0500

    added regex rule to API endpoint for matching objectId

--- Vendor/urls.py
+++ Vendor/urls.py
@@ -7,7 +7,9 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^articles/$', 'vendorApp.views.articles'),
-    url(r'^articles/popular/$', 'vendorApp.views.popular', name='popular'),
-    url(r'^articles/tech/$', 'vendorApp.views.tech', name='tech'),
+    url(r'^articles/$', 'vendorApp.views.get_articles'),
+    url(r'^articles/popular/$', 'vendorApp.views.get_popular', name='popular'),
+    url(r'^articles/tech/$', 'vendorApp.views.get_tech', name='tech'),
+    url(r'^artlice/like/(?P<article_id>[0-9a-fA-F]{24})/$', 'vendorApp.views.like_article', name='home'),
+    url(r'^artlice/dislike/(?P<article_id>[0-9a-fA-F]{24})/$', 'vendorApp.views.dislike_article', name='home'),
 )
