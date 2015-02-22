01cf8b0abac0bb2bfae10cbfa78e30d8cc055eb4
Kaosisochukwu Uzokwe <kuzokwe@gmail.com>
2015-02-22 17:14:58 -0500

    modified method names for retrieving articles list and tech/popular variations; added a method to handle requests for a single article; added methods for handling requests to like and dislike individual articles

--- vendorApp/views.py
+++ vendorApp/views.py
@@ -1,12 +1,12 @@
 import os
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework.decorators import api_view
-from rest_framework.decorators import renderer_classes
-from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view, renderer_classes
 from rest_framework.renderers import JSONRenderer
+from rest_framework.response import Response
 from models import Article
 from serializers import ArticleSerializer
-from mongoengine import connect
+from mongoengine import connect, DoesNotExist
 
 # connect to MongoDB
 conn_uri = os.environ.get('MONGOLAB_URI')
@@ -20,7 +20,7 @@ else:  # development env, using localhost
 @csrf_exempt
 @api_view(['GET'])
 @renderer_classes((JSONRenderer,))
-def articles(request):
+def get_articles(request):
     results = []
     for article in Article.objects:
         results.append(article)
@@ -31,9 +31,31 @@ def articles(request):
 @csrf_exempt
 @api_view(['GET'])
 @renderer_classes((JSONRenderer,))
-def popular(request):
+def get_article(request, article_id):
+    try:
+        article = Article.objects.get(id=article_id)
+    except DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    serializer = ArticleSerializer(article)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+@renderer_classes((JSONRenderer,))
+def get_popular(request):
     results = []
     for article in Article.objects(section='Popular'):
+        results.append(article)
+    serialized_list = ArticleSerializer(results, many=True)
+    return Response(serialized_list.data)
+
+
+@api_view(['GET'])
+@renderer_classes((JSONRenderer,))
+def get_tech(request):
+    results = []
+    for article in Article.objects(section='Tech'):
         results.append(article)
     serialized_list = ArticleSerializer(results, many=True)
     return Response(serialized_list.data)
@@ -42,9 +64,31 @@ def popular(request):
 @csrf_exempt
 @api_view(['GET'])
 @renderer_classes((JSONRenderer,))
-def tech(request):
-    results = []
-    for article in Article.objects(section='Tech'):
-        results.append(article)
-    serialized_list = ArticleSerializer(results, many=True)
-    return Response(serialized_list.data)
+def like_article(request, article_id):
+    try:
+        article = Article.objects.get(id=article_id)
+    except DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    article.popularity += 1
+    article.save()
+
+    serializer = ArticleSerializer(article)
+    return Response(serializer.data)
+
+
+@csrf_exempt
+@api_view(['GET'])
+@renderer_classes((JSONRenderer,))
+def dislike_article(request, article_id):
+    try:
+        article = Article.objects.get(id=article_id)
+    except DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if article.popularity > 0:
+        article.popularity -= 1
+        article.save()
+
+    serializer = ArticleSerializer(article)
+    return Response(serializer.data)
