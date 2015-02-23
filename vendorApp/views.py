1cad37300b81ae8c5063a16555ed91e7efc77ce9
Kaosisochukwu Uzokwe <kuzokwe@gmail.com>
2015-02-22 19:18:07 -0500

    added utility query function which calls the new utility sort function for sorting across multiple fields

--- vendorApp/views.py
+++ vendorApp/views.py
@@ -6,7 +6,7 @@ from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
 from models import Article
 from serializers import ArticleSerializer
-from mongoengine import connect, DoesNotExist
+from mongoengine import connect, DoesNotExist, Document
 
 # connect to MongoDB
 conn_uri = os.environ.get('MONGOLAB_URI')
@@ -17,14 +17,39 @@ else:  # development env, using localhost
     connect(db)
 
 
+# utility sorting function
+def multiKeySort(items, columns):
+    from operator import itemgetter
+    comparators = [((itemgetter(col[1:].strip()), -1) if col.startswith('-') else
+                    (itemgetter(col.strip()), 1)) for col in columns]
+
+    def comparator(left, right):
+        for fn, multiple in comparators:
+            result = cmp(fn(left), fn(right))
+            if result:
+                return multiple * result
+        else:
+            return 0
+    return sorted(items, cmp=comparator)
+
+
+# utility query function
+def get_articles_section(category=None):
+    results = []
+    if category is None:
+        for article in Article.objects:
+            results.append(article)
+    else:
+        for article in Article.objects(section=category):
+            results.append(article)
+    return multiKeySort(results, ['popularity', 'mixIndex', '-dateAdded'])
+
+
 @csrf_exempt
 @api_view(['GET'])
 @renderer_classes((JSONRenderer,))
 def get_articles(request):
-    results = []
-    for article in Article.objects:
-        results.append(article)
-    serialized_list = ArticleSerializer(results, many=True)
+    serialized_list = ArticleSerializer(get_articles_section(), many=True)
     return Response(serialized_list.data)
 
 
@@ -44,20 +69,14 @@ def get_article(request, article_id):
 @api_view(['GET'])
 @renderer_classes((JSONRenderer,))
 def get_popular(request):
-    results = []
-    for article in Article.objects(section='Popular'):
-        results.append(article)
-    serialized_list = ArticleSerializer(results, many=True)
+    serialized_list = ArticleSerializer(get_articles_section(category='Popular'), many=True)
     return Response(serialized_list.data)
 
 
 @api_view(['GET'])
 @renderer_classes((JSONRenderer,))
 def get_tech(request):
-    results = []
-    for article in Article.objects(section='Tech'):
-        results.append(article)
-    serialized_list = ArticleSerializer(results, many=True)
+    serialized_list = ArticleSerializer(get_articles_section(category='Tech'), many=True)
     return Response(serialized_list.data)
 
 
