b7b1cce9f5609cc73ee6c079e4809b6f4f3ae203
obinnaokwodu <obinnaokwodu.gmail.com>
2015-10-21 19:31:56 +0100

    Rolling Back to Normal (w/out home)

--- vendorApp/views.py
+++ vendorApp/views.py
@@ -53,18 +53,6 @@ def get_articles_by_region(region, limit, offset):
     query_size = len(base_query)
     return base_query.order_by('-dateAdded', 'mixIndex').skip(offset).limit(limit), query_size
 
-
-def get_home_articles(region, limit, offset):
-    master = []
-    size = 10
-    query_size = size*2
-    for x in range(0, size):
-        master.append(Article.objects(region=region, section="Headlines").order_by('-dateAdded', 'mixIndex')[x])
-        master.append(Article.objects(region=region, section="Gossip").order_by('-dateAdded', 'mixIndex')[x])
-    return master.skip(offset).limit(limit), query_size
-
-        
-
 def get_trending_by_region(region, limit, offset):
     today = datetime.datetime.today()
     age = datetime.timedelta(days=TRENDING_LIFESPAN)
