87117c001d85a3ee4c7103c21c8248f0f176675c
obinnaokwodu <emekaokwodu@NNAEMEKAs-MacBook-Pro.local>
2015-10-31 22:35:02 +0100

    trying 'none'

--- vendorApp/views.py
+++ vendorApp/views.py
@@ -247,7 +247,7 @@ def get_region_politics(request, region):
 
 @api_view(['GET'])
 @renderer_classes((JSONRenderer,))
-def get_region_section_articles(request, region, sectionA, sectionB, sectionC):
+def get_region_section_articles(request, region, sectionA, sectionB, sectionC=None):
     return Response(generate_section_output(get_section_articles_by_region, region, sectionA, sectionB, sectionC, request))  
 
 @api_view(['GET'])
