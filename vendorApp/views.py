c3c0b560fb88525fa1be67f1765d9c3748218f0b
obinnaokwodu <emekaokwodu@NNAEMEKAs-MacBook-Pro.local>
2015-11-02 13:48:08 +0100

    logos

--- vendorApp/views.py
+++ vendorApp/views.py
@@ -222,7 +222,7 @@ def generate_output_sectionwise(query_func, region,section, request):
 @api_view(['GET'])
 @renderer_classes((JSONRenderer,))
 def get_logo_all(request, region):
-    gossip = ArticleSerializer(get_region_logos_for_section_do(region,"Gossip")[0], many=True).data
+    gossip = get_region_logos_for_section_do(region,"Gossip")
     tech = ArticleSerializer(get_region_logos_for_section_do(region,"Tech")[0], many=True).data
     business = ArticleSerializer(get_region_logos_for_section_do(region,"Business")[0], many=True).data
     headlines = ArticleSerializer(get_region_logos_for_section_do(region,"Headlines")[0], many=True).data
