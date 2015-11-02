5e515b998909a9c34d74ed9363779d10439fc003
obinnaokwodu <emekaokwodu@NNAEMEKAs-MacBook-Pro.local>
2015-11-02 20:15:24 +0100

    more debugging

--- vendorApp/views.py
+++ vendorApp/views.py
@@ -224,10 +224,10 @@ def get_region_startup(request, region):
 @api_view(['GET'])
 @renderer_classes((JSONRenderer,))
 def get_logo_all(request, region):
-    gossip = LogoSerializer(get_region_logos_for_section_all(region), many=True).data
+    gossip = LogoSerializer(get_region_logos_for_section_all(region)[0], many=True).data
     
     content = {
-        "gossip": "Is this thing on",
+        "gossip": gossip,
     }
     
     return Response(content)
